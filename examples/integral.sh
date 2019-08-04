7c085ceeee1940148a30474de76b6f35c8e1cf14
iuvbio <lyndonbjohn@gmail.com>
2019-08-04 14:07:13 +0200

    improved examples

--- examples/integral.sh
+++ examples/integral.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pytex2svg="../tex2svg"
-formula="\int_{-\infty}^\infty e^{-x^2}\,dx = \sqrt{\pi}"
+equation="\int_{-\infty}^\infty e^{-x^2}\,dx = \sqrt{\pi}"
 
-"$pytex2svg" "$formula" -o "integral.svg"
+pytex2svg "$equation" -o "integral.svg"
